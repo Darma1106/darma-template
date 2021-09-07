476e6c59bc1e752be6ac3b7839fd95ae9a77ecd4
darma <1946498212@qq.com>
2021-09-07 23:35:31 +0800

    ci: 移除全局类型文件中无关的部分

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,7 @@
 declare module '*.vue' {
+  // eslint-disable-next-line import/named
   import { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
-}
-
-declare interface Array<T> {
-  clean: () => void
-  delete: (index: number) => T
 }
